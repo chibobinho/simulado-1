8cec2524dd8b91627e05c88985147b0f021aa5bb
Yuri Mitsugui Chiba <44440053866@info.scs>
2022-03-09 10:25:00 -0300

    saep primeiro teste

---
+++ TechMan-saep/Data/ContextoTech.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechMan_saep.Models;
+
+namespace TechMan_saep.Data
+{
+    public class ContextoTech : DbContext
+    {
+        public ContextoTech(DbContextOptions<ContextoTech> options)
+                   : base(options)
+        {
+        }
+
+        public DbSet<Perfis> Perfis { get; set; }
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public DbSet<Equipamentos> Equipamentos { get; set; }
+        public DbSet<Comentarios> Comentarios { get; set; }
+    }
+}
---
+++ TechMan-saep/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TechMan_saep.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
